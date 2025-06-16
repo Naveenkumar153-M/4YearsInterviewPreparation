a3e614d92185d1e6919885fb2040833d33551a6e
Naveenkumar M <naveenkumar153@outlook.com>
2025-06-16 23:32:19 +0530

    Adding Tata Elxsi Interview Question

--- src/test/java/selenium/GF.java
+++ src/test/java/selenium/GF.java
@@ -3,6 +3,7 @@ package selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.*;
 
@@ -19,24 +20,31 @@ public class GF extends Setup {
         driver.findElement(By.id("signInButton")).click();
         implicitWaitMethod();
 
+        try {
+            Thread.sleep(3000);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         cssElementToBeVisible("#nestedFiltersButton");
         cssClick("#nestedFiltersButton");
         cssElementToBeVisible("#categoryTrigger");
         cssClick("#categoryTrigger");
 
 
-        List<WebElement> webElementListOfCategory = driver.findElements(By.xpath("(//gf-shady-scroller)[2]//span[@class='stock-art-category-name']"));
+        List<WebElement> webElementListOfCategory = driver.findElements(By.xpath("(//gf-shady-scroller)[3]//span[@class='stock-art-category-name']"));
         int countOfCategory = webElementListOfCategory.size();
 
         Map<String, Integer> map = new HashMap<>();
+        int count=0;
 
         for (WebElement element : webElementListOfCategory) {
             element.click();
             List<WebElement> sub = driver.findElements(By.xpath("(//div[contains(@class,'second-level')]//div[@class='p-y-1'])[2]/mat-checkbox//label"));
-
             for (WebElement subElement : sub) {
                 String subCategory = subElement.getText().trim().toLowerCase();
                 map.put(subCategory, map.getOrDefault(subCategory, 0) + 1);
+                count++;
             }
         }
         System.out.println(map);
@@ -48,6 +56,7 @@ public class GF extends Setup {
             }
         }
         System.out.println(duplicationCount);
+        System.out.println("Count : "+count);
         browserCloseAndQuit();
 
     }
